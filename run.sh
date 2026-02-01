c43be2e7322b4dc2ecdb4175a77888fe3c8afb9c
Giang Nguyen <hoanggia@uni-bremen.de>
2026-02-01 20:14:22 +0100

    Fix run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ cd $(dirname $0) || exit
 docker run --rm --network host \
   -v "$(pwd)"/config.json:/etc/cloudflared/config.json \
   cloudflare/cloudflared:latest \
-  tunnel --no-autoupdate --config /etc/cloudflared/config.json run --token eyJhIjoiODY0Zjc4Y2ZiOTY3ZjFhNTExOGJhN2NlZDA1ZWZkMjgiLCJ0IjoiNDg4N2I3ZDQtNzc4ZC00MGRhLWI4MjctMjkxOWMwOTU0ZTdlIiwicyI6Ik9EQXhNek13TkdNdE9EWmxNQzAwWkdJNUxUZzFNak10TjJabFkyUTBORFV5WXpBeiJ9
+  tunnel --no-autoupdate --config /etc/cloudflared/config.json run --token eyJhIjoiODY0Zjc4Y2ZiOTY3ZjFhNTExOGJhN2NlZDA1ZWZkMjgiLCJ0IjoiYTJlOTUwNTItOWIxYy00MjI5LWFlNjQtNmE4OGU5YzQyYzBhIiwicyI6IlltVm1ORE01WmpFdE16QmtOaTAwTUdNNExXRTRPVEF0TmpNNU1XRTNNbU5oWkdFMyJ9
